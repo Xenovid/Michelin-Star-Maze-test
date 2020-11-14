9df6910a39f4cf5bb108cbbe467b4506e7bfeb04
David <cubsel@hotmail.com>
2020-11-14 09:34:51 -0500

    got a pause menu started
    pause menu is started, but there is a major bug that's preventing me from finishing it

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused;
+    public GameObject userInterface;
+    public GameObject puaseMenu;
+    public void Pause()
+    {
+        userInterface.SetActive(false);
+        puaseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        userInterface.SetActive(true);
+        puaseMenu.SetActive(false);
+        isPaused = false;
+        
+    }
+    private void Update()
+    {
+        if(Input.GetAxis("pause") == 1 && !(isPaused))
+        {
+            Pause();
+        }
+    }
+}
