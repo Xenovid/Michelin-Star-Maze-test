fb2b6c9f7527fd1c317df2b0364182b083afccce
ezraincorvaia <lmi32@drexel.edu>
2020-11-16 18:36:05 -0500

    Added Colliderbox to Table and created table function to add to

---
+++ Assets/Scripts/TableFunction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableFunction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        
+    }
