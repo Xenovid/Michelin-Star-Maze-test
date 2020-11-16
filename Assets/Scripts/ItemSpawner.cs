baafb31477d5899f03e46c0d3a8bbf753cef1af8
ezraincorvaia <lmi32@drexel.edu>
2020-11-16 16:34:26 -0500

    updates to pcg items

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -12,15 +12,28 @@ public class ItemSpawner : MonoBehaviour
 
     public void SpawnItems()
     {
+        templates.Tables[0].transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+
+        rand = UnityEngine.Random.Range(0, templates.newrooms.Count);
+        Instantiate(templates.Tables[1], new Vector3(templates.newrooms[rand].transform.position.x, 1.5f, templates.newrooms[rand].transform.position.z), templates.Tables[1].transform.rotation);
+
+        for (i = 0; i < 6; i++)
+        {
+            rand = UnityEngine.Random.Range(0, templates.newrooms.Count);
+            Instantiate(templates.Tables[0], new Vector3(templates.newrooms[rand].transform.position.x, 1.5f, templates.newrooms[rand].transform.position.z), templates.Tables[0].transform.rotation);
+        }
+        
+
         for (i = 0; i < templates.newrooms.Count; i++)
         {
             for (j = 0; j < 5; j++)
             {
                 UnityEngine.Random.seed = UnityEngine.Random.Range(0, 100);
                 rand = UnityEngine.Random.Range(0, templates.newrooms.Count);
-                Instantiate(templates.Foods[i], new Vector3(templates.newrooms[rand].transform.position.x+2, 1.5f, templates.newrooms[rand].transform.position.z-2), templates.Foods[0].transform.rotation);
+                Instantiate(templates.Foods[i], new Vector3(templates.newrooms[rand].transform.position.x+2, 1.7f, templates.newrooms[rand].transform.position.z-2), templates.Foods[0].transform.rotation);
             }
         }
+        
     }
 
     void Start()
