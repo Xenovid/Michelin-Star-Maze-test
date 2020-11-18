c80d087a2d2afc135cb636ac9692f6d13c20a758
ezraincorvaia <lmi32@drexel.edu>
2020-11-17 20:01:46 -0500

    Spawn scripting

--- Assets/Scripts/AddRoom.cs
+++ Assets/Scripts/AddRoom.cs
@@ -6,20 +6,18 @@ public class AddRoom : MonoBehaviour
 {
     private RoomTemplates templates;
     
-    int i;
-    int j;
-    int rand;
+
+   
 
     void Awake()
     {
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         templates.newrooms.Add(this.gameObject);
-    }
-
-
-    void Start() {
         
+        
+
     }
+
 
     
 
---
+++ Assets/Scripts/AddSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSpawn : MonoBehaviour
+{
+    private SpawnTemplates spawna;
+
+
+    void Start()
+    {
+        spawna = GameObject.FindGameObjectWithTag("Spawn Point").GetComponent<SpawnTemplates>();
+        spawna.spawns.Add(this.gameObject);
+    }
+
+    
+}
