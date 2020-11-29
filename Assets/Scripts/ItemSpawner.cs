5c54a3ed2b0cc74fbfaeb979be365cf2d948de96
ezraincorvaia <lmi32@drexel.edu>
2020-11-29 16:56:56 -0500

    Food Checker Translate Code

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -15,15 +15,20 @@ public class ItemSpawner : MonoBehaviour
     {
         int z = 0;
         templates.Tables[0].transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+
+        GameObject _gameObject;
         rand = (templates.newrooms.Count - 1);
-        //the food checker has a bunch on items that it needs to know(and arn't connected in the prefab), just inisilizing it will break the game
-        //Instantiate(templates.Tables[1], new Vector3(templates.newrooms[rand].transform.position.x, 1.5f, templates.newrooms[rand].transform.position.z), templates.Tables[1].transform.rotation);
+        _gameObject = GameObject.Find("tempfoodChecker");
+
+        _gameObject.transform.position = new Vector3(templates.newrooms[rand].transform.position.x, 1.5f, templates.newrooms[rand].transform.position.z);
+
         templates.newrooms.RemoveAt(rand);
         for (i = 0; i < 6; i++)
         {
-            rand = UnityEngine.Random.Range(0, templates.newrooms.Count);
+            rand = UnityEngine.Random.Range(0, templates.newrooms.Count-2);
             Instantiate(templates.Tables[0], new Vector3(templates.newrooms[rand].transform.position.x, 1.5f, templates.newrooms[rand].transform.position.z), templates.Tables[0].transform.rotation);
         }
+
         
 
         for (i = 0; i < templates.newrooms.Count; i++)
@@ -38,7 +43,7 @@ public class ItemSpawner : MonoBehaviour
             for (j = 0; j < 2; j++)
             {
                 UnityEngine.Random.seed = UnityEngine.Random.Range(0, 100);
-                rand = UnityEngine.Random.Range(0, templates.newrooms.Count-1);
+                rand = UnityEngine.Random.Range(0, templates.newrooms.Count-2);
                 //change i to j
                 Instantiate(templates.Foods[j], new Vector3(templates.newrooms[rand].transform.position.x+2, 1.7f, templates.newrooms[rand].transform.position.z-2), templates.Foods[0].transform.rotation);
                 Instantiate(templates.Lights[0], new Vector3(templates.newrooms[rand].transform.position.x + 2, 12f, templates.newrooms[rand].transform.position.z - 2), templates.Lights[0].transform.rotation);
